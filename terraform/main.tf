167a633ab6fb837560686aa8d73cc8a4b8ceee6e
Pawissanutt <pawissanutt@gmail.com>
2022-03-22 01:10:15 -0500

    Update README.adoc

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,8 +12,6 @@ terraform {
 provider "aws" {
   profile    = "default"
   region     = "us-east-1"
-  access_key = "EDIT_HERE"
-  secret_key = "EDIT_HERE"
 }
 
 resource "aws_key_pair" "deployer" {
